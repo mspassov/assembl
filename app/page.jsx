d0b88314e44868fc2ca83a93b28555294930900c
Martin Spassov <martispassov@gmail.com>
2025-12-20 18:43:49 -0500

    Added ingredient form + styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import IngredientForm from "@/components/IngredientForm";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <section className="container">
+      <h2>Add ingredients you already have, and get new recipes in seconds!</h2>
+      <IngredientForm />
+    </section>
+  );
+};
 
-export default HomePage
+export default HomePage;
