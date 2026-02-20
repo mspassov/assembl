fa309127dba006bb396c294db73e39285c7784a1
Martin Spassov <mspassov@uwo.ca>
2026-02-19 22:11:28 -0500

    Added sign in functionality with next-auth

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+"use client";
+import React, { use } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 import logo from "@/assets/logo.png";
 import "@/assets/nav.css";
 
 const Navbar = () => {
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+
+    setAuthProviders();
+  }, []);
+
+  const { data: sessionData } = useSession();
+
   return (
     <nav>
       <div className="container">
@@ -35,7 +51,28 @@ const Navbar = () => {
               </Link>
             </li>
             <li>
-              <button className="btn log-in-btn">Log In</button>
+              {!sessionData && (
+                <div>
+                  {providers &&
+                    Object.values(providers).map((provider, index) => (
+                      <button
+                        onClick={() => signIn(provider.id)}
+                        key={index}
+                        className="btn log-in-btn"
+                      >
+                        Log In / Register
+                      </button>
+                    ))}
+                </div>
+              )}
+              {sessionData && (
+                <div>
+                  <div>{sessionData.user.name}</div>
+                  <div>{sessionData.user.email}</div>
+                  <img src={sessionData.user.image} alt="" />
+                  <button className="btn log-out-btn">Log Out</button>
+                </div>
+              )}
             </li>
           </ul>
         </div>
