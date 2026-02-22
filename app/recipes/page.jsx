d2d304559ae25af5ebb39a0f6c4008e853517350
Martin Spassov <martispassov@gmail.com>
2026-02-21 23:16:35 -0500

    Added all recipes page

--- app/recipes/page.jsx
+++ app/recipes/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import connectDB from "@/config/database";
+import Recipe from "@/models/Recipe";
+import AllRecipeCard from "@/components/AllRecipeCard";
 
-const AllRecipesPage = () => {
-  return <div>AllRecipesPage</div>;
+const AllRecipesPage = async () => {
+  await connectDB();
+  const recipes = await Recipe.find({}).populate("author").lean();
+
+  return (
+    <section className="container-md recipes-grid">
+      {recipes.map((recipe) => (
+        <AllRecipeCard recipe={recipe} key={recipe._id} />
+      ))}
+    </section>
+  );
 };
 
 export default AllRecipesPage;
