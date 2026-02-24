2d52f73ffe9d26eb48d74fe461f5d546a3b48adb
Martin Spassov <martispassov@gmail.com>
2026-02-23 20:53:54 -0500

    Added styles to all recipe card

--- components/AllRecipeCard.jsx
+++ components/AllRecipeCard.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import "@/assets/allRecipeCard.css";
 import { useState } from "react";
 import { FaRegClock } from "react-icons/fa6";
@@ -22,42 +23,44 @@ const AllRecipeCard = ({ recipe }) => {
   };
 
   return (
-    <div className="all-recipe-card">
-      <Image
-        src={recipe.imgURL}
-        alt={recipe.recipeTitle}
-        height={100}
-        width={150}
-        className="all-recipe-card-img"
-      />
-      <div className="all-recipe-card-content">
-        <p className="title">
-          <strong>{recipe.recipeTitle}</strong>
-        </p>
-        <div className="recipe-badges">
-          <div className="time">
-            <FaRegClock />
-            <span>{recipe.cookTime} minutes</span>
+    <Link href={`/recipes/${recipe._id}`} className="recipe-link" target="_blank">
+      <div className="all-recipe-card">
+        <Image
+          src={recipe.imgURL}
+          alt={recipe.recipeTitle}
+          height={100}
+          width={150}
+          className="all-recipe-card-img"
+        />
+        <div className="all-recipe-card-content">
+          <p className="title">
+            <strong>{recipe.recipeTitle}</strong>
+          </p>
+          <div className="recipe-badges">
+            <div className="time">
+              <FaRegClock />
+              <span>{recipe.cookTime} minutes</span>
+            </div>
+            <div
+              className="difficulty"
+              style={{ backgroundColor: difficultyColour }}
+            >
+              {recipe.difficulty}
+            </div>
           </div>
-          <div
-            className="difficulty"
-            style={{ backgroundColor: difficultyColour }}
-          >
-            {recipe.difficulty}
-          </div>
+          <p className="num-ingredients">
+            Number of Ingredients: {recipe.allIngredients.length}
+          </p>
+          <p className="submission">
+            <i>Author: {recipe.author.username}</i>
+          </p>
+          <button onClick={handleClick} className="btn save-btn">
+            {!saved ? <FaRegHeart /> : <FaHeart className="saved-heart" />}
+            <span>Save</span>
+          </button>
         </div>
-        <p className="num-ingredients">
-          Number of Ingredients: {recipe.allIngredients.length}
-        </p>
-        <p className="submission">
-          <i>Author: {recipe.author.username}</i>
-        </p>
-        <button onClick={handleClick} className="btn save-btn">
-          {!saved ? <FaRegHeart /> : <FaHeart className="saved-heart" />}
-          <span>Save</span>
-        </button>
       </div>
-    </div>
+    </Link>
   );
 };
 
