1647a139d8fb5ed772b3a5b364e7f22d29d7ff5c
Martin Spassov <martispassov@gmail.com>
2026-02-28 10:17:04 -0500

    Completed the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer>
-      <div className="container">
-        <div>Developed by Martin</div>
+      <div className="container footer-container">
+        <Link
+          className="links source-container"
+          href="https://github.com/mspassov/assembl"
+          target="_blank"
+        >
+          <FaGithub /> <span>Source Code</span>
+        </Link>
+        <div>&copy; {year} Assembl Development. All rights reserved.</div>
+        <ul className="contact-container">
+          <li>
+            <Link
+              className="links"
+              href="https://github.com/mspassov/assembl"
+              target="_blank"
+            >
+              <FaLinkedin className="linkedin" />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://mspassov.vercel.app/"
+              target="_blank"
+              className="links portfolio"
+            >
+              Portfolio
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
